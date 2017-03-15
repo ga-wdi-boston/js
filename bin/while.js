c08ccdfe22f287408573ac4cd1b9e001552948c6
payne-chris-r <06cpayne@gmail.com>
2017-03-14 20:46:56 -0400

    Fixes code to make js-standard
    Ran make-standard to fix linter errors

--- bin/while.js
+++ bin/while.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-//We'll learn about require later in the course
-const ask = require('../lib/ask.js');
+// We'll learn about require later in the course
+const ask = require('../lib/ask.js')
 
-let count = 0;
-let answer = '';
+let count = 0
+let answer = ''
 
 while (answer !== 'Brian') {
-  answer = ask('Guess my name? ');
-  count = count + 1;
+  answer = ask('Guess my name? ')
+  count = count + 1
 }
-console.log('You got it in ' + count + ' tries!');
+console.log('You got it in ' + count + ' tries!')
